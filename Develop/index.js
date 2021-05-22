7d3e2e1dd529876810759e3a8f458891e2341ac6
marigut <mari.gutierrez96@yahoo.com>
2021-05-22 13:53:06 -0500

    questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,90 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generatePage = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the projects title? (Required)',
+      validate: titleInput => {
+        if (titleInput) {
+          return true;
+        } else {
+          console.log('Please enter project title!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Enter description of the project (Required)',
+      validate: descriptionInput => {
+        if (descriptionInput) {
+          return true;
+        } else {
+          console.log('Please enter description of the project!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Enter project installation instructions (Required)',
+      validate: installationInput => {
+        if (installationInput) {
+          return true;
+        } else {
+          console.log('Please enter installation instructions!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Enter usage infomation (Required)',
+      validate: usageInput => {
+        if (usageInput) {
+          return true;
+        } else {
+          console.log('Please enter usage information!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'contribution',
+      message: 'Enter contribution guidelines (Required)',
+      validate: contributionInput => {
+        if (contributionInput) {
+          return true;
+        } else {
+          console.log('Please enter contribution guidelines!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'test',
+      message: 'Enter test instructions (Required)',
+      validate: testInput => {
+        if (testInput) {
+          return true;
+        } else {
+          console.log('Please enter test instructions!');
+          return false;
+        }
+      }
+    }
+  ]);
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
